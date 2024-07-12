76e6a0f9752bf05e9a27b07ea3fd5c85ad16764a
Пакри <pakri@yandex.ru>
2024-07-13 00:17:15 +0300

    Правки.

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,13 +1,14 @@
 import { saveAccordionState } from './local-storage';
 
 const faqDetailsList = document.querySelector('.faq__details-list');
+const faqItems = document.querySelectorAll('.faq__tab-item');
 
 const onAccordionItemClick = (evt) => {
   const faqItem = evt.target.closest('li');
   if (faqItem) {
     const answer = faqItem.querySelector('p');
     answer.style.height = answer.style.height ? null : `${answer.scrollHeight}px`;
-    const activeTabIndex = Array.from(document.querySelectorAll('.faq__tab-item')).findIndex((item) => item.classList.contains('faq__tab-item--active'));
+    const activeTabIndex = Array.from(faqItems).findIndex((item) => item.classList.contains('faq__tab-item--active'));
     saveAccordionState(activeTabIndex);
   }
 };
