cdae9eba8eca6cd9d5be590b57fb0f1285ed26ff
Пакри <pakri@yandex.ru>
2024-07-13 20:39:19 +0300

    Закончил функционал swiper для блока review

--- source/js/swiper-reviews.js
+++ source/js/swiper-reviews.js
@@ -5,7 +5,7 @@ import '../sass/vendor/swiper.scss';
 const reviewsSwiperContainer = document.querySelector('.swiper-reviews');
 
 const reviewsSwiperInit = () => {
-  new Swiper(reviewsSwiperContainer, {
+  const reviewsSwiper = new Swiper(reviewsSwiperContainer, {
     modules: [Navigation],
     speed: 400,
     spaceBetween: 40,
@@ -16,8 +16,43 @@ const reviewsSwiperInit = () => {
       nextEl: '.swiper-button-reviews-next',
       prevEl: '.swiper-button-reviews-prev',
     },
+    on: {
+      slideChange: () => {
+        updateReviewsButtons();
+      },
+      reachEnd: () => {
+        updateReviewsButtons();
+      },
+      reachBeginning: () => {
+        updateReviewsButtons();
+      }
+    }
   });
+
+  function updateReviewsButtons() {
+    const prevButton = document.querySelector('.swiper-button-reviews-prev');
+    const nextButton = document.querySelector('.swiper-button-reviews-next');
+
+    if (reviewsSwiper.isBeginning) {
+      prevButton.classList.add('swiper-button-prev--disabled');
+      prevButton.disabled = true;
+    } else {
+      prevButton.classList.remove('swiper-button-prev--disabled');
+      prevButton.disabled = false;
+    }
+
+    if (reviewsSwiper.isEnd) {
+      nextButton.classList.add('swiper-button-prev--disabled');
+      nextButton.disabled = true;
+    } else {
+      nextButton.classList.remove('swiper-button-prev--disabled');
+      nextButton.disabled = false;
+    }
+  }
+  updateReviewsButtons();
 };
+
+//Поиск всех методов свайпера для лока кнопки занял 5 часов.
 
 export { reviewsSwiperInit };
 
