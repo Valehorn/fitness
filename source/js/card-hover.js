cd61a3ebc4fd8b632437714eba383e008115541b
Пакри <pakri@yandex.ru>
2024-07-19 16:45:17 +0300

    Доделал состояние для hover кнопки price через js

---
+++ source/js/card-hover.js
@@ -0,0 +1,19 @@
+const priceCardList = document.querySelector('.price__cards-list');
+
+const onCardHover = (evt) => {
+  const buttonTarget = evt.target.closest('.card__button');
+  const card = buttonTarget ? buttonTarget.closest('.card') : null;
+
+  if (!card) {
+    return;
+  }
+
+  card.classList.toggle('card--hover');
+};
+
+const cardHover = () => {
+  priceCardList.addEventListener('mouseover', onCardHover);
+  priceCardList.addEventListener('mouseout', onCardHover);
+};
+
+export { cardHover };
--- source/js/main.js
+++ source/js/main.js
@@ -4,11 +4,13 @@ import { reviewsSwiperInit } from './swiper-reviews';
 import { faqTabsToggle } from './faq-tabs';
 import { faqAccordionToggle } from './accordion';
 import { changePriceTab } from './price-tab';
+import { cardHover } from './card-hover';
 import './validate-form';
 
 const bootStrap = () => {
   openVideo();
   changePriceTab();
+  cardHover();
   juriSwiperInit();
   reviewsSwiperInit();
   faqTabsToggle();
